243545ddd364b4b5c663e3352cb8529135694f42
Konstantin Trufanov <tehkucher@gmail.com>
2017-06-11 13:44:45 +0200

    basic class sceleton

--- tircbot.py
+++ tircbot.py
@@ -1,11 +1,42 @@
 #tircbot module for znc
 
-import znc
+#import znc
+import json
 
 
-class Tircbot(znc.Module):
+highlighted_file = 'teleirc.db'
+
+class tircbot():
     desciption = "ZNC module for telegram bot"
 
+# On init we need only API key to send messages to telegram bot
+
     def __init__(self, api_key):
-        api_key = None
+        self.api_key = api_key
+        self.highlightedList = self.getHighlightedList()
+
+
+    def OnChanMsg(self, Nick, Channel, sMessage):
+
+        if sMessage in self.highlightedList:
+            pass
+            # send message to proper channel and highlight it
+        else:
+            # just forward to proper telegram channel
+            pass
+
+    def  getHighlightedList(self):
+
+        with open(highlighted_file, 'r') as f:
+            return f.read()
+        # here we will get the list of hl strings to compare
+
+    def addHighlightedString(self):
+        # add some strings to db
+        pass
+
+    def receiver(self):
+        pass
+
+        # here we will be listening on messages we need to forward
 
