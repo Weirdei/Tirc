81e26f35613649a30bd29af0a7f9089bbfd6bf6f
Konstantin Trufanov <tehkucher@gmail.com>
2017-06-13 17:28:11 +0200

    testing bot

--- bottests.py
+++ bottests.py
@@ -1,7 +1,35 @@
 import telepot
+from telepot.loop import MessageLoop
+from pprint import pprint
 
-bot = telepot.Bot('323157985:AAFmJt2zJh5Kyf3FxL3yJ-sVzUb4jhpASsI')
+API_KEY = '323157985:AAH6_R0PyQemilBWZ4RsV9ttzfvDDqgourA'
 
-print(bot.getMe())
+MESSAGE_ID_CONTAINER_FILENAME = 'message_id_container'
 
-print(bot.getUpdates())
+
+def read_update_id(filename):
+    with open(filename) as f:
+        return int(f.read())
+
+
+def write_update_id(filename, num):
+    with open(filename, 'w') as f:
+        f.write(num)
+
+
+bot = telepot.Bot(API_KEY)
+
+# temporary container aka list for storing all received messages
+
+# Receive message later then highest_update_id, return tuple (list_of_messages,highest_update_id)
+def message_receiver(update_id):
+    msg = bot.getUpdates(offset=update_id)
+    update_id_container = []
+    for i in msg:
+        update_id_container.append(i['update_id'])
+    if update_id_container:
+        update_id = (max(update_id_container) + 1)
+    return msg, update_id
+
+def message_sender(nick_of_sender,message_to_send):
+    pass
